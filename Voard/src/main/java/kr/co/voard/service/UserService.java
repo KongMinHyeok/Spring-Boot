c0e0bd240345aae9f2dc0852b46d4ddd80855fb3
mh990407 <dlwlzjffj1@naver.com>
2023-04-26 11:56:07 +0900

    2023/04/26 terms/register/login 백엔드 실습

---
+++ Voard/src/main/java/kr/co/voard/service/UserService.java
@@ -0,0 +1,51 @@
+package kr.co.voard.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import kr.co.voard.dao.UserDAO;
+import kr.co.voard.repository.UserRepo;
+import kr.co.voard.vo.TermsVO;
+import kr.co.voard.vo.UserVO;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserDAO dao;
+	
+	@Autowired
+	private UserRepo repo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	
+	public TermsVO selectTerms() {
+		
+		return dao.selectTerms(); 
+	}
+	
+	
+	public int insertUser(UserVO vo) {
+		
+		vo.setPass(passwordEncoder.encode(vo.getPass2()));
+		
+		int result = dao.insertUser(vo);
+		
+		return result;
+		
+	}
+	
+	public int countUser(String uid) {
+		return repo.countByUid(uid);
+	}
+	
+	public void selectUser() {}
+	public void selectUsers() {}
+	public void updateUser() {}
+	public void deleteUser() {}
+	
+}
